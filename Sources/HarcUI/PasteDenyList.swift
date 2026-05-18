6506017bb38246edefcbb0a6d40c3027eb902e3b
J <jlanefc@icloud.com>
2026-05-18 09:02:03 -0700

    Fix library recording selection and paste prompts

--- Sources/HarcUI/PasteDenyList.swift
+++ Sources/HarcUI/PasteDenyList.swift
@@ -11,6 +11,7 @@ public enum PasteDenyList {
         "com.apple.finder",
         "com.apple.systempreferences",
         "com.apple.ScreenSaver.Engine",
+        "com.harc.Harc",
         "com.agilebits.onepassword7",
         "com.agilebits.onepassword8",
         "com.bitwarden.desktop",
@@ -24,6 +25,7 @@ public enum PasteDenyList {
     public static let lockedBundleIDs: Set<String> = [
         "com.apple.loginwindow",
         "com.apple.ScreenSaver.Engine",
+        "com.harc.Harc",
         "com.agilebits.onepassword7",
         "com.agilebits.onepassword8",
         "com.bitwarden.desktop",
